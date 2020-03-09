8a090c73506b2ebb98fe062c7c73c92f6225b882
Gregor <gregorlennie@hotmail.co.uk>
2020-03-09 21:42:24 +0000

    Adds sharedPreferences for product name

--- app/src/main/java/com/example/inventoryapplication/MainActivity.kt
+++ app/src/main/java/com/example/inventoryapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.inventoryapplication
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,9 @@ class MainActivity : AppCompatActivity() {
         goToAddProduct.setOnClickListener {
             startActivity(Intent(this, AddProductActivity::class.java))
         }
+
+        val preferences = getSharedPreferences("database", Context.MODE_PRIVATE)
+        val savedName = preferences.getString("savedProductName", "This value doesn't exist")
+        d("gregor", "Saved message is: $savedName")
     }
 }
