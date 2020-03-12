3865d7fb6bd8f23e2e5efdc9cc6bacd27281c078
Gregor <gregorlennie@hotmail.co.uk>
2020-03-12 20:18:39 +0000

    Adds coroutine to fetch from Url

--- app/src/main/java/com/example/inventoryapplication/MainActivity.kt
+++ app/src/main/java/com/example/inventoryapplication/MainActivity.kt
@@ -5,7 +5,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
+import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +21,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, AddProductActivity::class.java))
         }
 
-        val preferences = getSharedPreferences("database", Context.MODE_PRIVATE)
-        val savedName = preferences.getString("savedProductName", "This value doesn't exist")
-        d("gregor", "Saved message is: $savedName")
 
-        lastSavedProduct.text = savedName
+        lifecycleScope.launch(Dispatchers.Default) {
+            val specialMessage = URL("https://finepointmobile.com/api/inventory/v1/message").readText()
+            d("gregor", "Special message is: $specialMessage")
+            lastSavedProduct.text = specialMessage
+        }
+
     }
 }
